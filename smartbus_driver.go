f06f7d7cb38a37e832fd2265c9ad82976e6bfe8d
Ivan Shvedunov <ivan4th@gmail.com>
2015-05-25 22:12:58 +0300

    Support -debug flag.

--- smartbus_driver.go
+++ smartbus_driver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"./smartbus"
 	"flag"
+	"github.com/contactless/wbgo"
 	"time"
 )
 
@@ -10,7 +11,11 @@ func main() {
 	serial := flag.String("serial", "/dev/ttyNSC1", "serial port address (/dev/... or host:port)")
 	broker := flag.String("broker", "tcp://localhost:1883", "MQTT broker url")
 	gw := flag.Bool("gw", false, "Provide UDP gateway")
+	debug := flag.Bool("debug", false, "Enable debugging")
 	flag.Parse()
+	if *debug {
+		wbgo.SetDebuggingEnabled(true)
+	}
 	if driver, err := smartbus.NewSmartbusTCPDriver(*serial, *broker, *gw); err != nil {
 		panic(err)
 	} else {
